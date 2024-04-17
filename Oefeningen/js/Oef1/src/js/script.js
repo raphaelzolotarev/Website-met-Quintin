0ea961c99873ad9346e2a03caa71371045a54651
raphaelzolotarev <raphaelzolotarev@gmail.com>
2024-04-18 00:06:30 +0200

    Project done

--- Oefeningen/js/Oef1/src/js/script.js
+++ Oefeningen/js/Oef1/src/js/script.js
@@ -1,111 +1,188 @@
-/*VARIABLES*/
+/*########### VARIABLES INIT ###########*/
 
-    let dices = [];
-    let numberOfDuos = 0;
-    let objDuoCounter = {
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0
-    };
+let dices = [];
+let numberOfDices = 5;
+let highestDiceNumber = 6;
+let worpen = 0;
+let objDiceOccurenceCounter;
+let  objStats = {
+        dubbel:0,
+        drievoudig:0,
+        viervoudig:0,
+        vijfvoudig:0
+    }; 
+let lastLaunch; 
+let objStatsKeys = Object.keys(objStats); 
+
+let audio = new Audio('src/media/dices.mp3');
+
+/*########### END OF VARIABLES INIT ###########*/
+
+
+
+
+
+/*########### DOM CONTENT ###########*/
+
+    //DIV WITH DICES IMAGES
+    let dicesDom = document.querySelector("#stenen"); 
+    initializeDicesDom(false);
+
+    //STATS
+    let statsDom = document.querySelector("table"); 
+    initializeStatsDom(false);
 
     //BUTTON
-    let button = document.querySelector("button");
-    button.addEventListener('click', event => {
-        event.preventDefault();
-        resetDices();
+    let buttonDom = document.querySelector("button");
+    buttonDom.addEventListener('click', event => {
+        audio.play();
+        launchDices();
+        countMatch();        
+        initializeDicesDom(true);
+        initializeStatsDom(true);
+        initializeWorpenDom(++worpen);
+        initializeLastLaunchDom();
+        getInfo();
     });
 
-/*########### END OF VARIABLES ###########*/
+    //WORPEN
+    let worpenDom = document.querySelector("#worpen");
+    initializeWorpenDom();
+
+    //LAST LAUNCH
+    let lastLaunchDom = document.querySelector("#last-launch");
+
+/*########### END OF DOM CONTENT ###########*/
 
 
 
-/*FUNCTIONS*/
 
-    //FUNCTION GET RANDOM DICES NUMBERS
-    function getRandomDice() {
-        let randomValue = Math.floor(Math.random() * 5) + 1;
-        switch(randomValue){
-            case 1: ++objDuoCounter[1];  break;
-            case 2: ++objDuoCounter[2];  break;
-            case 3: ++objDuoCounter[3];  break;
-            case 4: ++objDuoCounter[4];  break;
-            case 5: ++objDuoCounter[5];  break;
-            case 6: ++objDuoCounter[6];  break;
-        }
-        return randomValue;
-    }
 
-    //FUNCTION NUMBER OF DUOS COUNTER
-    function countDuos() {
-        numberOfDuos=0;
-        let resultaat = "Je hebt ";
-        let typeOfWins ={ 
-                dubbel: 0,
-                driedubbel: 0,
-                viervoudig: 0,
-                vijfvoudig: 0,
-                perfect: 0
-        }
-        
-        for(nbr in objDuoCounter){
-            switch(objDuoCounter[nbr]){
-                case 2: ++typeOfWins.dubbel; break;
-                case 3: ++typeOfWins.driedubbel; break;
-                case 4: ++typeOfWins.viervoudig; break;
-                case 5: ++typeOfWins.vijfvoudig; break;
-                case 6: ++typeOfWins.perfect; break;
-            }
-        }
-        let currentWins = typeOfWins.filter(
-            function(value) { 
-                return value>0; 
-            }
-        );
-        for(k in currentWins){
-            resultaat+="<span>"+currentWins[k]+"</span> "+k;
-        }            
+/*########### FUNCTIONS ###########*/
 
-        let numberOfDuosDom = document.querySelector("#number_of_duos");
-        numberOfDuosDom.innerHTML = resultaat;
-    }
+    //LAUNCH DICES
+    function launchDices(){
 
-    //FUNCTION GET INFOS
-    function getInfos() {
-        console.log(dices);
-        console.log(objDuoCounter);
-    }
-
-    //FUNCTION RESET DICES ARRAY AND DOM
-    function resetDices() {
-
-        //CLEAN DATAS FOR NEW GAME
+        //1. Clear first
         dices = [];
-        for (key in objDuoCounter) {
-            objDuoCounter[key] = 0;
+
+        //2. Add 6 random values
+        for(let i = 0; i<numberOfDices; ++i){
+            dices.push(Math.floor(Math.random() * highestDiceNumber) + 1);
         }
 
-        //ADD RANDOM NBRS TO ARRAY
-        for (let i = 0; i < 5; ++i) {
-            dices.push(getRandomDice());
+    }
+
+
+
+    //INITIALIZE DICES IMAGES IN DOM
+    function initializeDicesDom(dicesLaunched){
+        if(!dicesLaunched){  
+            //CLEAR
+            dicesDom.innerHTML = "";  
+            //ADD           
+            for(let i = 0; i<numberOfDices; ++i){
+                dicesDom.innerHTML += "<img src='src/img/dobbel"+ highestDiceNumber + ".gif'>"; 
+            }
+        } else{
+            //CLEAR
+            dicesDom.innerHTML = "";  
+            //ADD        
+            for(let i = 0; i<numberOfDices; ++i){
+                dicesDom.innerHTML += "<img src='src/img/dobbel"+ dices[i] + ".gif'>"; 
+            }
+        }
+    }
+
+
+
+    //INITIALIZE STATS IN DOM
+    function initializeStatsDom(dicesLaunched){
+        if(!dicesLaunched){  
+            //CLEAR
+            statsDom.innerHTML = "";  
+            //ADD           
+            for(let i = 1; i<numberOfDices; ++i){
+                statsDom.innerHTML += '<tr><td>Max ' + (i + 1) + ' gelijke: </td><td>0</td></tr>';
+            }
+        } else{
+            //CLEAR
+            statsDom.innerHTML = "";  
+            //ADD  
+            for(k in objStats){ 
+                statsDom.innerHTML += '<tr><td>Max ' + (objStatsKeys.indexOf(k)+2) + ' gelijke: </td><td>'+objStats[k]+'</td></tr>';
+            }
+        }
+    }
+
+
+
+    //INITIALIZE STATS IN DOM
+    function initializeWorpenDom(){
+        worpenDom.innerHTML = worpen+" worpen";
+    }
+
+
+
+    //INITIALIZE LAST LAUNCH IN DOM
+    function initializeLastLaunchDom(){
+        let resultaat = "Je hebt ";
+        for(k in lastLaunch){
+            if(lastLaunch[k]!=0){
+                resultaat += "<b style='color:red;'>"+lastLaunch[k]+"</b> <b>"+k+"</b>, ";
+            }
+        }
+        lastLaunchDom.innerHTML = resultaat == "Je hebt " ? "Je hebt niks" : resultaat.slice(0, -2)+" gegooid";
+    }
+
+
+
+    //INITIALIZE DICES IMAGES IN DOM
+    function countMatch(){
+        lastLaunch = [];
+        objDiceOccurenceCounter = {
+            1:0,
+            2:0,
+            3:0,
+            4:0,
+            5:0,
+            6:0
+        };    
+        lastLaunch = {
+            dubbel:0,
+            drievoudig:0,
+            viervoudig:0,
+            vijfvoudig:0
+        };     
+        for(let i = 0; i<dices.length; ++i){
+            switch(dices[i]){
+                case 1: ++objDiceOccurenceCounter[1]; break;
+                case 2: ++objDiceOccurenceCounter[2]; break;
+                case 3: ++objDiceOccurenceCounter[3]; break;
+                case 4: ++objDiceOccurenceCounter[4]; break;
+                case 5: ++objDiceOccurenceCounter[5]; break;
+                case 6: ++objDiceOccurenceCounter[6]; break;
+            } 
+        }
+        for(k in objDiceOccurenceCounter){
+            switch(objDiceOccurenceCounter[k]){
+                case 2: ++objStats.dubbel;         ++lastLaunch.dubbel;          break;
+                case 3: ++objStats.drievoudig;     ++lastLaunch.drievoudig;      break;
+                case 4: ++objStats.viervoudig;     ++lastLaunch.viervoudig;      break;
+                case 5: ++objStats.vijfvoudig;     ++lastLaunch.vijfvoudig;      break;
+            } 
         }
 
-        //REMOVE CURRENT DICES FROM DOM
-        let dicesDom = document.querySelector("#stenen");
-        dicesDom.innerHTML = "";
+    } 
 
-        //ADD NEW DICES IN DOM
-        for (let dice of dices) {
-            dicesDom.innerHTML += `
-                <img src="src/img/dobbel`+ dice + `.gif" width="50px"> 
-            `;
-        }
 
-        //SHOW NUMBER OF DUOS IN DOM
-        countDuos();
-        getInfos();    
+
+    //INFO
+    function getInfo(){
+        console.log(dices);
+        console.log(objDiceOccurenceCounter);
+        console.log(objStats);
+        console.log(lastLaunch);
     }
 
 /*########### END OF FUNCTIONS ###########*/
